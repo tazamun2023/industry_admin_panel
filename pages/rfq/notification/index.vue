7cbfb382cde5ca42dc491125a06010e3e0059ff8
Ziaur Rahaman <45318029+Ziaurrahaman7@users.noreply.github.com>
2024-01-09 22:02:18 +0300

    rfq design vendor

---
+++ pages/rfq/notification/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <!-- ---------------- -->
+    <div class="row  mb-2">
+        <div class="card p-20 col-md-12">
+                <div class="card-body">
+                    <div class="row mt-2">
+                        <div class="col-md-12">
+                            <h4 class="bold black">RFQ Notifications</h4>
+                            <p class="font-14">Set your preferences to receive a customised list of Requests For Quotations (RFQ) periodically. Your specific preferences on categories and keywords will bring you the most recent RFQs on the marketplace.</p>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+    <!-- --------------------- -->
+    <div class="row mb-2">
+        <div class="card p-20 col-md-12">
+            <h4>How do you want to receive the RFQ list?</h4>
+        </div>
+    </div>
+        <!-- --------end-------- -->
+    </div>
+</template>
+
+<script>
+
+</script>
