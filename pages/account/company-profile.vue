74c45ca6a1b0c03f3b2fc592b6a06f088076b55b
Ziaur Rahaman <45318029+Ziaurrahaman7@users.noreply.github.com>
2024-02-10 16:41:59 +0300

    logo inserted

--- pages/account/company-profile.vue
+++ pages/account/company-profile.vue
@@ -175,6 +175,19 @@
             <h4>Primary Information</h4>
           </div>
           <div class="form-group">
+            <div  class="input-wrapper mb-2">
+             <!-- component -->
+             <label for="">Logo Upload</label>
+          <div class="flex w-full items-center justify-center bg-grey-lighter">
+              <label class="w-64 flex flex-col items-center px-4 py-6 bg-white text-primary rounded-lg shadow-lg tracking-wide uppercase border border-primary cursor-pointer hover:bg-primary hover:text-white">
+                  <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                      <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+                  </svg>
+                  <span class="mt-2 text-base leading-normal">Select a file</span>
+                  <input type='file' class="hidden" />
+              </label>
+          </div>
+            </div>
             <div class="input-wrapper mb-2">
               <label for="">{{ $t('vendor.foundation_date') }}</label>
               <input type="date"  v-model="fromData.founded_date" readonly>
