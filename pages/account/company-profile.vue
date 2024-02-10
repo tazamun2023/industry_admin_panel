752d7e879d3c4c6ae0ea2bbe3dfad4b877bbc364
Ziaur Rahaman <45318029+Ziaurrahaman7@users.noreply.github.com>
2024-02-10 16:49:24 +0300

    ar problem solved

--- pages/account/company-profile.vue
+++ pages/account/company-profile.vue
@@ -34,7 +34,7 @@
             <lang-input :hasError="hasError" type="text" :title="$t('global.name')" :valuesOfLang="result.name"
                   @updateInput="updateInput">
                 </lang-input>
-            <lang-input :hasError="hasError" type="textarea" :title="$t('vendor.details')" :valuesOfLang="result.name"
+            <lang-input :hasError="hasError" type="textarea" :title="$t('vendor.details')" :valuesOfLang="result.detail"
                   @updateInput="updateInput"></lang-input>
               <!-- <ValidationProvider class="w-full" name="Name Arabic" rules="max:30|required" v-slot="{ errors }">
             <div class="input-wrapper w-full mb-2">
@@ -179,7 +179,7 @@
              <!-- component -->
              <label for="">Logo Upload</label>
           <div class="flex w-full items-center justify-center bg-grey-lighter">
-              <label class="w-64 flex flex-col items-center px-4 py-6 bg-white text-primary rounded-lg shadow-lg tracking-wide uppercase border border-primary cursor-pointer hover:bg-primary hover:text-white">
+              <label class="w-64 flex flex-col items-center px-4 py-6 bg-white text-primary rounded-lg shadow-lg tracking-wide uppercase border border-smooth cursor-pointer hover:bg-primary hover:text-white">
                   <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                       <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
                   </svg>
@@ -469,7 +469,7 @@ export default {
       emailModal: false,
       result:{
         name: {'ar': '', 'en': ''},
-        detail:''
+        detail: {'ar': '', 'en': ''},
       },
        fromData:{
              id:'',
@@ -576,7 +576,10 @@ export default {
       // Remove the email input field at the specified index
       this.phones.splice(index, 1);
     },
+    updateInput(input, language, value) {
 
+this.$set(input, language, value);
+},
   },
 
  async mounted() {
