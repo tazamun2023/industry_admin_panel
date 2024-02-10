e1a7a93ad9442feb7ba0b4103876abf33fd03f25
Osama Al-mamari <43526628+OsamaAlmamri@users.noreply.github.com>
2024-02-10 15:01:57 +0300

    update brands

--- pages/brands/_id.vue
+++ pages/brands/_id.vue
@@ -5,12 +5,10 @@
     set-api="setBrand"
     get-api="getBrand"
     empty-store-variable="allBrands"
-    set-image-api="setBrandImage"
     route-name="brands"
     :name="$t('prod.brand')"
     gate="brand"
     :validation-keys="['title', 'slug']"
-    :file-keys="['id', 'status']"
     :result="result"
     @result="result = $event"
   >
@@ -18,24 +16,40 @@
     <template v-slot:form="{hasError}">
 
       <div class="input-wrapper">
-        <label>{{ $t('index.title') }}</label>
-        <input
-          type="text"
-          :placeholder="$t('index.title')"
-          name="title"
-          v-model="result.title"
-          ref="title"
-          @change="slugChange"
-          :class="{invalid: !!!result.title && hasError}"
-        >
+        <label>{{ $t('index.image') }}</label>
+        <upload-files @updateInput="saveAttachment" :max-files="1"></upload-files>
         <span
           class="error"
           v-if="!!!result.title && hasError"
         >
-          {{ $t('category.req', { type: $t('index.title')}) }}
+          {{ $t('category.req', {type: $t('index.image')}) }}
         </span>
       </div>
 
+
+      <div class="input-wrapper">
+        <label>{{ $t('index.title') }}</label>
+<!--        <input-->
+<!--          type="text"-->
+<!--          :placeholder="$t('index.title')"-->
+<!--          name="title"-->
+<!--          v-model="result.title"-->
+<!--          ref="title"-->
+<!--          @change="slugChange"-->
+<!--          :class="{invalid: !!!result.title && hasError}"-->
+<!--        >-->
+
+        <lang-input :hasError="hasError" type="text" :title="$t('index.title')" :valuesOfLang="result.title"
+                    @updateInput="updateInput"></lang-input>
+
+
+        <span
+          class="error"
+          v-if="!!!result.title && hasError"
+        >
+          {{ $t('category.req', {type: $t('index.title')}) }}
+        </span>
+      </div>
 
 
       <div class="input-wrapper">
@@ -52,14 +66,14 @@
           class="error"
           v-if="!!!result.slug && hasError"
         >
-          {{ $t('category.req', { type: $t('category.slug')}) }}
+          {{ $t('category.req', {type: $t('category.slug')}) }}
         </span>
       </div>
 
       <div class="input-wrapper">
         <div class="dply-felx j-left mb-20 mb-sm-15">
           <span class="mr-15">
-            {{$t('category.featured')}}
+            {{ $t('category.featured') }}
           </span>
 
           <dropdown
@@ -91,46 +105,62 @@
 
 <script>
 
-  import Dropdown from "~/components/Dropdown"
-  import DataPage from "~/components/partials/DataPage"
-  import util from "~/mixin/util"
-  import {mapGetters} from 'vuex'
+import Dropdown from "~/components/Dropdown"
+import DataPage from "~/components/partials/DataPage"
+import util from "~/mixin/util"
+import {mapGetters} from 'vuex'
+import UploadFiles from "../../components/UploadFiles.vue";
+import LangInput from "../../components/langInput.vue";
 
-  export default {
-    name: "brands",
-    middleware: ['common-middleware', 'auth'],
-    data() {
-      return {
-        result: {
-          id: '',
-          title: '',
-          slug: '',
-          featured: 2,
-          status: 2,
-          image: ''
-        }
+export default {
+  name: "brands",
+  middleware: ['common-middleware', 'auth'],
+  data() {
+    return {
+      result: {
+        id: '',
+        // title: '',
+        title: {'ar': '', 'en': ''},
+        slug: '',
+        featured: 2,
+        status: 2,
+        image: '',
+        file: '',
       }
-    },
-    mixins: [util],
-    components: {
-      DataPage,
-      Dropdown
-    },
-    computed: {
-      ...mapGetters('language', ['currentLanguage']),
-    },
-    methods: {
-      featuredSelected(data) {
-        this.result.featured = data.key
-      },
-      dropdownSelected(data) {
-        this.result.status = data.key
-      },
-
-    },
-    async mounted() {
     }
+  },
+  mixins: [util],
+  components: {
+    LangInput,
+    UploadFiles,
+    DataPage,
+    Dropdown
+  },
+  computed: {
+    ...mapGetters('language', ['currentLanguage']),
+  },
+  methods: {
+    updateInput(input, language, value) {
+
+      this.$set(input, language, value);
+    },
+    featuredSelected(data) {
+      this.result.featured = data.key
+    },
+    dropdownSelected(data) {
+      this.result.status = data.key
+    },
+    saveAttachment(image) {
+      console.log(image)
+      // this.result.rfq_attachments = rfq_attachments
+      this.result.file = image
+    },
+
+  },
+
+  async mounted() {
   }
+}
 </script>
 
 <style scoped>
