cbca7c8f8d5c8233783c1fb2415a319c96ac075f
Zillur Rahman Noyon <36861551+zillurcse@users.noreply.github.com>
2024-02-20 17:31:06 +0300

    fix products issues

--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -1258,6 +1258,7 @@ export default {
         overview: '',
         description: {ar: '', en: ''},
         status: '',
+        is_quote: '',
         brand_id: 0,
         primary_category_id: '',
         category_id: '',
@@ -1572,6 +1573,9 @@ export default {
         this.result.unit_id = null
       }
       this.result.status = 'pending'
+      this.result.is_quote = false
+      this.result.is_variant = false
+
       this.checkForm()
     },
 
