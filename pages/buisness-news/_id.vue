8f44e1ec96d797b6a201c4ef04ac25a85a597e98
Zillur Rahman Noyon <36861551+zillurcse@users.noreply.github.com>
2024-02-28 16:08:14 +0600

    done

--- pages/buisness-news/_id.vue
+++ pages/buisness-news/_id.vue
@@ -1,61 +1,91 @@
 <template>
-  <div class="tab-sidebar">
-    <div class="card">
-      <div class="title p-2 border-b border-smooth flex justify-between">
-        <h3>Add Buisness News </h3>
-        <nuxt-link :to="`/buisness-news`" class="button primary-btn">List</nuxt-link>
+  <data-page
+    ref="dataPage"
+    set-api="NEWS"
+    get-api="NEWS"
+    method="put"
+    route-name="buisness-news"
+    :name="$t('bnews.news')"
+    gate="brand"
+    :validation-keys="['title.ar','title.en', 'short_description.ar', 'short_description.en']"
+    :result="result"
+    @result="resultData"
+  >
+    <template v-slot:form="{hasError}">
+      <div class="tab-sidebar">
+        <div class="card">
+<!--          <div class="title p-2 border-b border-smooth flex justify-between">-->
+<!--            <h3>Add Buisness News </h3>-->
+<!--            <nuxt-link :to="`/buisness-news`" class="button primary-btn">List</nuxt-link>-->
+<!--          </div>-->
+          <form class="p-4" action="">
+            <div class="my-2 input-wrapper">
+              <!--          <input type="text" placeholder="Title here">-->
+              <lang-input :hasError="hasError" type="text" :title="$t('bnews.News Title')" :valuesOfLang="result.title"
+                          @updateInput="updateInput"></lang-input>
+            </div>
+            <div class="input-wrapper my-2">
+              <!--          <textarea name="" id="" cols="30" rows="10"></textarea>-->
+              <lang-input :hasError="hasError" type="textarea" :title="$t('bnews.Short Details')"
+                          :valuesOfLang="result.short_description"
+                          @updateInput="updateInput"></lang-input>
+            </div>
+            <div class="input-wrapper my-2">
+              <!--          <textarea name="" id="" cols="30" rows="10"></textarea>-->
+              <lang-input :hasError="hasError" type="textarea" :title="$t('bnews.News Details')"
+                          :valuesOfLang="result.description"
+                          @updateInput="updateInput"></lang-input>
+            </div>
+            <div class="my-2 input-wrapper">
+              <img :src="result.image" alt="" style="height: 200px; width: 200px">
+              <label for="">Upload Image</label>
+              <upload-files @updateInput="saveAttachment"></upload-files>
+              <!--          <input type="file" name="" id="">-->
+            </div>
+          </form>
+        </div>
       </div>
-      <form class="p-4" action="">
-        <div class="my-2 input-wrapper">
-          <!--          <input type="text" placeholder="Title here">-->
-          <lang-input :hasError="hasError" type="text" :title="$t('bnews.News Title')" :valuesOfLang="result.title"
-                      @updateInput="updateInput"></lang-input>
-        </div>
-        <div class="input-wrapper my-2">
-<!--          <textarea name="" id="" cols="30" rows="10"></textarea>-->
-          <lang-input :hasError="hasError" type="textarea" :title="$t('bnews.News Details')"
-                      :valuesOfLang="result.description"
-                      @updateInput="updateInput"></lang-input>
-        </div>
-        <div class="my-2 input-wrapper">
-          <label for="">Upload Image</label>
-          <upload-files @updateInput="saveAttachment"></upload-files>
-<!--          <input type="file" name="" id="">-->
-        </div>
-        <div class="text-end">
-          <button class="button primary-btn" @click.prevent="doSubmit">Save</button>
-        </div>
-      </form>
-    </div>
-  </div>
+    </template>
+  </data-page>
 </template>
 
 <script>
 import LangInput from "@/components/langInput.vue";
+import DataPage from "@/components/partials/DataPage.vue";
 
 export default {
   name: 'BusinessNews',
 
-  components:{
-    LangInput
+  components: {
+    DataPage,
+    LangInput,
   },
 
   data() {
     return {
       hasError: false,
       setApi: 'setBusinessNews',
-      result:{
+      result: {
+        id: '',
+        attachment_type: 'file_upload',
+        vendor_id: this.$store.getters["admin/profile"].vendor_id,
         title: {ar: '', en: ''},
+        short_description: {ar: '', en: ''},
         description: {ar: '', en: ''},
-        images: []
+        image: [],
+        keywords: [],
+        file: '',
       }
     }
   },
 
   methods: {
-    doSubmit(){
-
-      this.checkForm()
+    resultData(evt) {
+      if (this.$route?.params?.id === 'add') {
+        // this.emptyAllList('allCategories')
+        this.$router.push({path: `/${this.routeName}${this.redirect ? '' : '/buisness-news'}`})
+      }
+      this.result = evt
     },
     updateInput(input, language, value) {
       this.$set(input, language, value);
@@ -63,31 +93,9 @@ export default {
     saveAttachment(images) {
       console.log(images)
       // this.result.rfq_attachments = rfq_attachments
-      this.result.images = images
+      this.result.file = images
+      // this.result.file = images
     },
-
-    async checkForm() {
-
-      // this.redirectingEnable(event.submitter.name)
-      this.formSubmitting = true
-      try {
-
-        delete this.result.created_at
-        delete this.result.updated_at
-        const data = await this.setById({id: this.id, params: {result: this.result}, api: this.setApi})
-        // if (data) {
-        //
-        //   this.result = Object.assign({}, data)
-        //   // this.$router.push({path: `/${this.routeName}${this.redirect ? '' : '/' + this.result.id}`})
-        //   this.$router.push({path: `/${this.routeName}${this.redirect ? '' : '/'}`})
-        // }
-        this.$router.push({path: `/${this.routeName}${this.redirect ? '' : '/buisness-news'}`})
-      } catch (e) {
-        return this.$nuxt.error(e)
-      }
-      this.formSubmitting = false
-    },
-
   },
 
   computed: {
--- pages/video/_id.vue
+++ pages/video/_id.vue
@@ -1,121 +1,87 @@
 <template>
   <data-page
     ref="dataPage"
-    set-api="setCity"
-    get-api="getCity"
-    set-image-api="setCityImage"
-    route-name="cities"
+    set-api="VideoGallery"
+    get-api="VideoGallery"
     method="put"
-    :name="$t('city.city')"
+    route-name="video"
+    :name="$t('global.video')"
     gate="brand"
-    :validation-keys="['name.ar','name.en','country_id']"
+    :validation-keys="['url']"
     :result="result"
     @result="resultData"
   >
     <template v-slot:form="{hasError}">
-
-      <lang-input :hasError="hasError" type="text" :title="$t('global.name')" :valuesOfLang="result.name"
-                  @updateInput="updateInput"></lang-input>
-
-
-      <div class="input-wrapper">
-        <span class="mr-15">{{ $t('title.select_country') }}</span>
-        <v-select
-          :dir="$t('app.dir')"
-          v-model="result.country_id"
-          :options="allCountries"
-          label="name"
-          :reduce="c => c.id"
-          :placeholder="$t('title.select_country')"
-          class="custom-select"
-        ></v-select>
-
-        <span
-          class="error"
-          v-if="!!!result.country_id && hasError"
-        >
-          {{ $t('category.req', {type: $t('city.country')}) }}
-        </span>
-      </div>
-
-
-      <div class="input-wrapper">
-        <div class="dply-felx j-left mb-20 mb-sm-15">
-            <span class="mr-15">
-              {{ $t('global.status') }}
-            </span>
-
-          <dropdown
-            :selectedKey="`${result.status}`"
-            :options="statusObj"
-            @clicked="dropdownSelected"
-          />
-        </div>
+      <div class="tab-sidebar">
+        <form class="p-4" action="">
+          <div class="my-2 input-wrapper">
+            <!--          <input type="text" placeholder="Title here">-->
+            <input class="form-control" name="e.g. Macbook Pro 2019" :placeholder="$t('global.url')" type="text"
+                   v-model="result.url">
+          </div>
+        </form>
       </div>
     </template>
   </data-page>
 </template>
 
 <script>
-
-import DataPage from "~/components/partials/DataPage";
-import util from "~/mixin/util"
-import Dropdown from '~/components/Dropdown'
-import {mapGetters, mapActions} from 'vuex'
-import LangInput from "../../components/langInput.vue";
+import LangInput from "@/components/langInput.vue";
+import DataPage from "@/components/partials/DataPage.vue";
 
 export default {
-  name: "Cities",
-  middleware: ['common-middleware', 'auth'],
+  name: 'BusinessNews',
+
+  components: {
+    DataPage,
+    LangInput,
+  },
+
   data() {
     return {
+      hasError: false,
+      setApi: 'setBusinessNews',
       result: {
         id: '',
-        name: {'ar': '', 'en': ''},
-        country_id: '',
-        status: '',
+        attachment_type: 'file_upload',
+        vendor_id: this.$store.getters["admin/profile"].vendor_id,
+        title: {ar: '', en: ''},
+        short_description: {ar: '', en: ''},
+        description: {ar: '', en: ''},
+        image: [],
+        keywords: [],
+        file: '',
       }
     }
   },
-  mixins: [util],
-  components: {
-    LangInput,
-    DataPage,
-    Dropdown
-  },
-  computed: {
-    ...mapGetters('language', ['currentLanguage']),
-    ...mapGetters('common', ['allCountries'])
-  },
+
   methods: {
-
-    updateInput(input, language, value) {
-
-      this.$set(input, language, value);
-    },
-    dropdownSelected(data) {
-      this.result.status = data.key
-    },
     resultData(evt) {
       if (this.$route?.params?.id === 'add') {
-        this.emptyAllList('allCategories')
+        // this.emptyAllList('allCategories')
+        this.$router.push({path: `/${this.routeName}${this.redirect ? '' : '/buisness-news'}`})
       }
       this.result = evt
     },
-
-    ...mapActions('common', ['getAllCountries', 'emptyAllList'])
+    updateInput(input, language, value) {
+      this.$set(input, language, value);
+    },
+    saveAttachment(images) {
+      console.log(images)
+      // this.result.rfq_attachments = rfq_attachments
+      this.result.file = images
+      // this.result.file = images
+    },
   },
-  async mounted() {
-    try {
-      await this.getAllCountries({api: 'getAllCountries', mutation: 'SET_ALL_COUNTRIES'})
-    } catch (e) {
-      return this.$nuxt.error(e)
-    }
 
+  computed: {
+    isAdding() {
+      return isNaN(this.$route?.params?.id)
+    },
+
+    id() {
+      return !this.isAdding ? this.$route?.params?.id : ''
+    },
   }
 }
 </script>
-
-<style scoped>
-
-</style>
