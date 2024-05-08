ae850ca0da66fdeb4a268bcb0bd7c06adb20c615
Osama Al-mamari <43526628+OsamaAlmamri@users.noreply.github.com>
2024-05-08 16:25:02 +0300

    set vendor shipment lines

---
+++ components/partials/ListEditPage.vue
@@ -0,0 +1,326 @@
+<template>
+  <div :class="{loading: loading}">
+    <div
+      v-if="deleting"
+      class="spinner-wrapper"
+    >
+      <spinner
+        :radius="60"
+        color="primary"
+        class="mr-15"
+      />
+    </div>
+    <div v-if="filter">
+      <slot
+        name="table-top"
+        v-bind:orderOptions="orderOptions"
+      >
+
+      </slot>
+    </div>
+    <transition
+      v-if="!gate || $can(gate)"
+      name="fade" mode="out-in"
+    >
+      <div v-if="!loading">
+        <div class="card">
+          <div class="table-wrapper">
+            <form
+              @submit.prevent="formSubmit"
+              :class="{'has-error': hasError}"
+            >
+            <table class="mn-w-600x">
+              <slot
+                name="table"
+                v-bind:list="list"
+              />
+            </table>
+
+            <div
+              class="dply-felx j-right single-btn my-2"
+            >
+
+              <ajax-button
+                name="save"
+                class="primary-btn"
+                :text="$t('setting.sv')"
+                :fetching-data="formSubmitting "
+              />
+            </div>
+            </form>
+          </div>
+        </div>
+        <!--        <div class="flex justify-between">-->
+        <!--          <h5 class="mt-20 mt-sm-15">{{ resultText }}</h5>-->
+        <!--          <pagination-->
+        <!--            :total-page="totalPage"-->
+        <!--          />-->
+        <!--      </div>-->
+
+      </div>
+      <shimmer
+        v-else
+      />
+    </transition>
+
+  </div>
+</template>
+
+<script>
+import Shimmer from '~/components/Shimmer'
+import util from '~/mixin/util'
+import {mapGetters, mapActions} from 'vuex'
+import Pagination from "~/components/partials/Pagination"
+import TableTop from "~/components/partials/TableTop"
+import routeParamHelper from "~/mixin/routeParamHelper"
+import Spinner from "~/components/Spinner"
+
+export default {
+  name: "ListEditPage",
+  props: {
+    addButton: {
+      type: Boolean,
+      default: true
+    },
+    method: {
+      type: String,
+      default: 'post'
+    },
+
+    setApi: {
+      type: String,
+      default: ''
+    },
+    modalButton: {
+      type: Boolean,
+      default: false
+    },
+    filter: {
+      type: Boolean,
+      default: true
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    id: {
+      type: String,
+      default: ''
+    },
+    param: {
+      type: Object,
+      default: null
+    },
+    gate: {
+      type: String,
+      default: null
+    },
+    manage_gate: {
+      type: String,
+      default: null
+    },
+    listApi: {
+      type: String,
+      default: ''
+    },
+    deleteApi: {
+      type: String,
+      default: ''
+    },
+    routeName: {
+      type: String,
+      default: ''
+    },
+    emptyStoreVariable: {
+      type: String,
+      default: null
+    },
+    orderOptions: {
+      type: Object,
+      default() {
+        return {
+          created_at: {title: this.$t('category.date')},
+          title: {title: this.$t('index.title')},
+          status: {title: this.$t('category.status')}
+        }
+      }
+    },
+  },
+  data() {
+    return {
+      formSubmitting: false,
+      deleting: false,
+      hasError: false,
+      loading: true,
+      result: null,
+    }
+  },
+  components: {
+    Spinner,
+    Pagination,
+    TableTop,
+    Shimmer
+  },
+  mixins: [util, routeParamHelper],
+  computed: {
+    resultText() {
+      if (this.result) {
+        if (this.result?.total > 0) {
+          return this.$t('list.show', {from: this.result?.from, to: this.result?.to, total: this.result?.total})
+        }
+        return this.$t('list.noData', {data: this.name})
+      }
+      return this.$t('list.loadn') + '...'
+    },
+    list() {
+      return this.result?.data
+    },
+    totalPage() {
+      return this.result?.last_page
+    },
+    ...mapGetters('language', ['currentLanguage']),
+  },
+  methods: {
+    ...mapActions('common', ['getById', 'setById']),
+
+    formSubmit() {
+      if (this.emitBeforeSubmit) {
+        this.$emit('before-submit')
+      } else {
+        this.checkForm()
+      }
+    },
+    async checkForm() {
+      // if (this.validationKeys.findIndex((i) => {
+      //   const parts = i.split(".");
+      //   if (i.includes("."))
+      //     return (!this.result[parts[0]][parts[1]])
+      //   else
+      //     return (!this.result[i])
+      // }) > -1) {
+      //   this.hasError = true
+      //   return false
+      // }
+
+      // this.redirectingEnable(event.submitter.name)
+      this.formSubmitting = true
+      try {
+
+        const data = await this.setById({id: this.id, params: {data:this.result.data}, api: this.setApi,method:this.method})
+        if (data) {
+
+          this.emptyAllList(this.emptyStoreVariable)
+
+         this.result=data
+          this.$emit('list', data)
+
+        } else {
+          this.$refs["formWrapper"].scrollIntoView({behavior: "smooth"})
+        }
+      } catch (e) {
+        return this.$nuxt.error(e)
+      }
+      this.formSubmitting = false
+
+    },
+    openModal() {
+      this.$emit('open-modal')
+    },
+    deleteBulk() {
+      this.$emit('delete-bulk')
+    },
+    async fetchingData() {
+      try {
+        this.settingRouteParam()
+        this.loading = true
+        this.result = await this.getRequest({
+          params: {
+            ...this.param,
+            ...this.$route.query,
+            ...this.listParams,
+            ...{time_zone: this.timeZone}
+          },
+          api: this.listApi
+        })
+        this.$emit('list', this.list)
+
+        this.loading = false
+      } catch (e) {
+        return this.$nuxt.error(e)
+      }
+    },
+    editItem(id) {
+      return this.$router.push(`/${this.routeName}/${id}`)
+    },
+    async deleteItem(id) {
+      if (confirm(this.$t('admin.dltMsg'))) {
+        try {
+          this.deleting = true
+          await this.deleteData({params: id, api: this.deleteApi})
+          this.emptyAllList(this.emptyStoreVariable)
+          this.$emit('deleted')
+          this.deleting = false
+          await this.fetchingData()
+        } catch (e) {
+          return this.$nuxt.error(e)
+        }
+      }
+    },
+    async deleteItemWithModal(id) {
+      try {
+        this.deleting = true
+        await this.deleteData({params: id, api: this.deleteApi})
+        this.emptyAllList(this.emptyStoreVariable)
+        this.$emit('deleted')
+        this.deleting = false
+        await this.fetchingData()
+      } catch (e) {
+        return this.$nuxt.error(e)
+      }
+    },
+    async deleteContentItem(id) {
+      try {
+        this.deleting = true
+        await this.deleteData({params: id, api: this.deleteApi})
+        this.emptyAllList(this.emptyStoreVariable)
+        this.$emit('deleted')
+        this.deleting = false
+        await this.fetchingData()
+      } catch (e) {
+        return this.$nuxt.error(e)
+      }
+    },
+
+    async deleteBankItem(id, params) {
+
+      alert("sdfdf")
+
+      try {
+        this.deleting = true
+        await this.deleteBank({
+          id: id,
+          params: {...params},
+          api: 'DeleteVendorBank'
+        })
+        this.emptyAllList(this.emptyStoreVariable)
+        this.$emit('deleted')
+        this.deleting = false
+        await this.fetchingData()
+      } catch (e) {
+        return this.$nuxt.error(e)
+      }
+    },
+
+    ...mapActions('common', ['deleteData', 'getRequest', 'emptyAllList', 'deleteBank'])
+  },
+  mounted() {
+    if (!this.gate || this.$can(this.gate)) {
+      this.fetchingData()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/shipping/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <list-edit-page
+    ref="listPage"
+    list-api="getSaShipping"
+    delete-api="deleteShippingRule"
+    set-api="storeSaFreeShipping"
+    method="post"
+    route-name="shipping-rules"
+    empty-store-variable="allShippingRules"
+    :name="$t('brand.shipRule')"
+    :order-options="orderOptions"
+    @delete-bulk="deleteBulk"
+    @list="itemList = $event"
+  >
+    <template v-slot:table-top>
+
+      <p class="button primary-btn">{{ $t('shipping.est') }}</p>
+    </template>
+
+    <template v-slot:table="{list}">
+      <tr class="lite-bold">
+        <th class="">
+          {{ $t('shipping.free_shipping') }}
+        </th>
+        <th>{{ $t('city.city') }}</th>
+        <th>{{ $t('shipping.est_days') }}</th>
+        <th>{{ $t('shipping.est_hours') }}</th>
+        <!--        <th>{{ $t('shipping.price') }}</th>-->
+      </tr>
+
+      <tr v-for="(value, index) in list" :key="index">
+        <td>
+          <input type="checkbox" :value="1" v-model="value.status">
+        </td>
+        <td>
+          <input type="hidden" v-model="value.id">
+          {{ value.city_name }}
+        </td>
+        <td><input type="number" min="0" v-model="value.est_days"></td>
+        <td><input type="number" min="0" max="23" v-model="value.est_hours"></td>
+        <!--        <td><input type="number" v-model="value.default_price"></td>-->
+
+      </tr>
+    </template>
+
+  </list-edit-page>
+</template>
+
+<script>
+import ListPage from "~/components/partials/ListPage";
+import util from '~/mixin/util'
+import bulkDelete from "~/mixin/bulkDelete";
+import DeleteButtonIcon from "../../components/partials/DeleteButtonIcon.vue";
+import EditButtonIcon from "../../components/partials/EditButtonIcon.vue";
+import ListEditPage from "../../components/partials/ListEditPage.vue";
+import {mapActions} from "vuex";
+
+export default {
+  name: "categories",
+  middleware: ['common-middleware', 'auth'],
+  data() {
+    return {
+      orderOptions: {
+        title: {title: this.$t('index.title')},
+        created_at: {title: this.$t('category.date')}
+      }
+    }
+  },
+  components: {
+    ListEditPage,
+    ListPage,
+    DeleteButtonIcon,
+    EditButtonIcon
+  },
+  mixins: [util, bulkDelete],
+  computed: {},
+  methods: {
+
+
+
+
+
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
