7fad10d1941d857a2785062bec842a20361b399a
Osama Al-mamari <43526628+OsamaAlmamri@users.noreply.github.com>
2024-06-06 20:27:10 +0300

    froala editor

---
+++ plugins/froala_editor.js
@@ -0,0 +1,10 @@
+
+
+import Vue from 'vue';
+import 'froala-editor/js/froala_editor.pkgd.min.js';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/css/froala_style.min.css';
+
+import VueFroala from 'vue-froala-wysiwyg';
+
+Vue.use(VueFroala);
