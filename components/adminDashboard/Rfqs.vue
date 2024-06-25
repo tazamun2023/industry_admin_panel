2971f4fc03e5613e74beb1063dbb0d14ce473386
Ziaur Rahaman <ziaurrahaman7info@gmail.com>
2024-06-25 16:33:08 +0600

    admin panel dashboard

---
+++ components/adminDashboard/Rfqs.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="p-4 card lg:h-[471px]">
+    <div class="flex justify-between">
+          <h4 class="text-[20px] font-medium">RFQs</h4>
+          <a href="" class="flex gap-4 items-center">{{ $t('Dashboard.More') }} <img class="w-5 h-5" src="~/assets/icon/next-g.svg" alt=""></a>
+      </div>
+    <div class="overflow-x-auto min-h-[214px] my-4 scroolbar">
+      <table>
+        <tbody>
+        <tr>
+          <td class="text-[13px] px-4 capitalize">
+            All RFQs
+          </td>
+          <td class="ltr:text-end px-4 rtl:text-start">53</td>
+        </tr>
+        <tr>
+          <td class="text-[13px] px-4 capitalize">
+            Pending
+          </td>
+          <td class="ltr:text-end px-4 rtl:text-start">55</td>
+        </tr>
+        <tr>
+          <td class="text-[13px] px-4 capitalize">
+            Approve
+          </td>
+          <td class="ltr:text-end px-4 rtl:text-start">6</td>
+        </tr>
+        <tr>
+          <td class="text-[13px] px-4 capitalize">
+            Quote Received
+          </td>
+          <td class="ltr:text-end px-4 rtl:text-start">43</td>
+        </tr>
+        <tr>
+          <td class="text-[13px] px-4 capitalize">
+            Quote Accepted
+          </td>
+          <td class="ltr:text-end px-4 rtl:text-start">33</td>
+        </tr>
+        <tr>
+          <td class="text-[13px] px-4 capitalize">
+            Cancelled
+          </td>
+          <td class="ltr:text-end px-4 rtl:text-start">4</td>
+        </tr>
+        <tr>
+          <td class="text-[13px] px-4 capitalize">
+            Rejected
+          </td>
+          <td class="ltr:text-end px-4 rtl:text-start">40</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <div>
+      <button class="flex gap-2 items-center justify-center text-white bg-theem h-[42px] text-[13px]"><img
+        class="w-[11px] h-[11px]" src="~/assets/icon/reload.svg" alt=""> Refresh Enhance
+      </button>
+    </div>
+  </div>
+</template>
+
---
+++ components/adminDashboard/Vendor.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="p-4 card lg:h-[471px]">
+        <div class="flex justify-between">
+            <h4 class="text-[20px] font-medium">Vendor</h4>
+            <a href="" class="flex gap-4 items-center">{{ $t('Dashboard.More') }} <img class="w-5 h-5" src="~/assets/icon/next-g.svg" alt=""></a>
+        </div>
+        <div class="my-2 chart">
+          <user-chart/>
+        </div>
+        <div class="grid grid-cols-2 py-3  gap-4">
+            <div class="border-r border-offwhite px-4">
+                <p class="flex  justify-between pb-2 gap-4 items-center hover:text-primary cursor-pointer">
+                  <span class="text-[14px] flex gap-4 items-center"><img class="w-[8px] h-[8px]" src="~/assets/icon/live.svg" alt=""> Approve</span>
+                <span class="text-[14px] font-bold">30</span>
+                <span class="text-[14px] text-[#8E95A9] hover:text-primary">15.3%</span>
+                </p>
+                <p class="flex pt-2 justify-between gap-4 items-center hover:text-orangeDeep  cursor-pointer">
+                  <span class="flex gap-4 text-[#8E95A9] hover:text-orangeDeep items-center"><img class="w-[8px] h-[8px]" src="~/assets/icon/adminelips.svg" alt=""> Un-Approve</span>
+                  <span class="text-[14px] font-bold">100</span>
+                  <span class="text-[14px] text-[#8E95A9] hover:text-orangeDeep">30.7%</span>
+                </p>
+            </div>
+            <div class="px-2">
+              <p class="flex  justify-between pb-2 gap-4 items-center hover:text-orangeLight cursor-pointer">
+                  <span class="text-[14px] flex gap-4 items-center text-[#8E95A9] hover:text-orangeLight"><img class="w-[8px] h-[8px]" src="~/assets/icon/orangeelips.svg" alt=""> Pending</span>
+                <span class="text-[14px] font-bold">450</span>
+                <span class="text-[14px] text-[#8E95A9] hover:text-orangeLight">54%</span>
+                </p>
+            </div>
+        </div>
+    </div>
+
+  </div>
+</template>
+<script>
+import UserChart from './UserChart.vue';
+
+export default {
+  components: {
+    UserChart,
+  },
+};
+</script>
+<style scoped>
+  .chart >>> canvas{
+    width: 650px !important;
+    height: 320px !important;
+  }
+</style>
