21db1b7dc24c433f77a613231036902473161e2f
Osama Al-mamari <43526628+OsamaAlmamri@users.noreply.github.com>
2024-06-26 21:19:04 +0300

    last update for polices

--- pages/account/polices.vue
+++ pages/account/polices.vue
@@ -5,21 +5,28 @@
       <div class="flex justify-between mt-6  mb-2">
         <div>
           <h4>{{ $t('polices.return_polices') }}</h4>
-          <p class="text-[.75em] mx-2 text-accent">{{$t('polices.minWords',{count:30})}}</p>
+          <p class="text-[.75em] mx-2 text-accent">{{ $t('polices.minWords', {count: 30}) }}</p>
         </div>
 
-        <img class="action_img cursor-pointer"
-             @click="showEditPolice=true"
-             src="~/assets/icon/edit-g.svg">
+        <div class="flex justify-between">
+
+
+          <img class="action_img cursor-pointer"
+               @click="showEditPolice=true"
+               src="~/assets/icon/edit-g.svg">
+        </div>
+
 
       </div>
       <hr>
       <div v-if="return_polices" class="my-5">
-        <div v-html="return_polices[$t('app.local')]"></div>
+        <h4 class="text-primary my-1">{{ $t('polices.LastUpdate') }}: {{ return_polices?.updated_at }}</h4>
+        <div v-html="return_polices.police[$t('app.local')]"></div>
 
       </div>
 
     </div>
+
 
     <EditPolices v-if="$can('edit_company')"
                  policeType="vendor"
@@ -48,7 +55,7 @@ export default {
   },
   mixins: [util],
   computed: {
-    ...mapGetters('admin', ['return_polices']),
+    ...mapGetters('admin', ['return_polices', 'profile']),
   },
   mounted() {
   },
